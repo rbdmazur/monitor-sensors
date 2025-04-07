e05a466cb6eaa801cab6bc2fb837078997a330a1
Daniil Zhuk <rbdmazur@gmail.com>
2025-04-07 14:38:37 +0300

    first commit

---
+++ src/main/java/by/rbdmazur/monitorsensor/MonitorSensorApplication.java
@@ -0,0 +1,13 @@
+package by.rbdmazur.monitorsensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSensorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorSensorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/rbdmazur/monitorsensor/MonitorSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package by.rbdmazur.monitorsensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
